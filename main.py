0f021c701e2e339402c5eb3ebbc3a2d2e5d31d11
lowbrain <83059975+lowbrain@users.noreply.github.com>
2023-08-03 03:29:20 +0000

    とりあえず基礎的なロジックが完成

--- main.py
+++ main.py
@@ -1,75 +1,108 @@
 import json
 import re
 import sys
-import os
 
-def main(type, filepath):
-    # 第１引数のConfigを読み込み
+#
+# main
+#
+def main(args):
+    # 必須引数の読み込み
     try:
-        cfg = Config(type)
+        cfg = readcfg('config.json', args[1])
+        defFile = open(args[2],'r')
+    except FileNotFoundError as err:
+        print(f'第２引数に指定したファイル({args[2]})が見つかりません。')
+        return
     except RuntimeError as err:
         print(err)
         return
 
-    # 第２引数の設定ファイルの読み込み
-    try:
-        file = open(filepath,'r')
-    except FileNotFoundError as err:
-        print(f'第２引数に指定したファイル({filepath})が見つかりません。')
-        return
+    # 第３引数以降の読み込み
+    consts = dict()
+    for const in args[3:]:
+        result = re.search('^(.+)=(.+)', const)
+        if result:
+            consts[result.groups(0)[0]] = result.groups(0)[1]
     
-    # ファイル解析
-    record = ''
-    while True:
-        line = file.readline()
-        if line == '':
+    # メインの処理を実行
+    extractDefItem(cfg, defFile, consts)
+    defFile.close()
+
+#
+# configファイルを読み込み指定の形式に該当するjsonを返却
+#
+def readcfg(cfgFilePath, type):
+    cfgFile = open(cfgFilePath, 'r')
+    jsoncfg = json.load(cfgFile)
+    cfgFile.close()
+
+    cfg = None
+    for define in jsoncfg['define']:
+        if define['type'] == type:
+            cfg = define
             break
-        elif re.match(cfg.inRecStart, line):
+
+    if cfg == None:
+        raise RuntimeError(f'指定のtype({type})がconfigファイル({cfgFilePath})に存在しません。')
+    
+    return cfg
+
+#
+# 設定ファイルの値をconfigファイルの定義をもとに抽出
+#
+def extractDefItem(cfg, defFile, consts):
+    print(cfg['output']['header'])
+    record = ''
+    while True: 
+        line = defFile.readline()
+        if line == '':
+            # ファイルの最終行だったらループを抜ける
+            break
+        elif re.match(cfg['input']['record-start'], line):
+            # 開始レコードを示す文字列を発見したら文字列バッファに文字列を格納
             record = line
         elif len(record) != 0:
-            if re.match(cfg.inRecEnd, line):
+            if re.match(cfg['input']['record-end'], line):
+                # 終了レコードを示す文字列を発見したらレコードを抽出し出力
                 record += line
-#                print(record)
-                items = []
-                for item in cfg.inRecInput:
-                    result = re.search(item, record, re.MULTILINE)
-#                    print(f'{item} - {result}')
-                    if result:
-                        items.append(result.group(1))
-                    else:
-                        items.append('')
-                for i, output in enumerate(items):
-                    print(output)
+                items = extractItem(record, cfg, consts)
+                print(cfg['output']['record'].format(*tuple(items)))
                 record = ''
             else: 
+                # 途上レコード文字列であったら文字列バッファに文字列を格納
                 record += line
-    file.close()
 
-class Config:
-    def __init__(self, type):
-        cfgFile = open('config.json', 'r')
-        jsoncfg = json.load(cfgFile)
-
-        cfg = None
-        for define in jsoncfg['define']:
-            if define['type'] == type:
-                cfg = define
-                break
-
-        if cfg != None:
-            self.inRecStart = define['input']['record-start']
-            self.inRecEnd = define['input']['record-end']
-            self.inRecInput = define['input']['record-item']
-            self.outHeader = define['output']['header']
-            self.outRecord = define['output']['record']
+#
+# レコードの抽出
+#
+def extractItem(record, cfg, consts):
+    items = []
+    for item in cfg['input']['record-item']:
+        if re.search('\(.+\)', item):
+            # 抽出対象
+            result = re.search(item, record, re.MULTILINE)
+            if result:
+                items.append(result.group(1))
+            else:
+                items.append('')
         else:
-            raise RuntimeError(f'第１引数に指定した設定ファイルの形式({type})が見つかりません。')
+            # 定数
+            if item in consts:
+                items.append(consts[item])
+            else:
+                items.append('')
+    return items
 
+#
+# 
+#
 args = sys.argv
 if __name__ == '__main__':
     if len(args) < 3:
-        print('引数が足りません。')
-        print('第１引数に設定ファイルの形式を、第２引数に設定ファイルのファイルパスを指定してください。')
-        print('ex) python mq /home/user/dmpmqmcfg.def')
+        print(f'Useage: python {args[0]} type filepath [const-value]...')
+        print(f'  type:        filepathの形式に基づき\"config.json\"に定義されるtypeの値を指定')
+        print(f'  filepath:    読み込む定義ファイルのパスを指定')
+        print(f'  const-value: 当プログラムにて扱いたい定数をname=value形式で指定')
+        print(f'  ex) python {args[0]} mq /home/user/dmpmqmcfg.def HOSNNAME=myhostname ITEM1=value1')
     else:
-        main(args[1], args[2])
+        main(args)
